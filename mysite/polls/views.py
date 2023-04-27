6d79f5485efff2ac182264b3c788e6b28a0d1d8d
Manishita Dey <deymani2752.9119@gmail.com>
2023-04-27 17:39:21 +0000

    Django FOr Everybody initial commit

---
+++ mysite/polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. f073b7fd is  the polls index.")
+# Create your views here.
